425189acd8ebba59f4c1790fd940a1ae99d14e3d
Katielly Santos <katygamesof@gmail.com>
2025-06-02 12:14:24 -0300

    Update cronometro.py

--- 3. Cronômetro/cronometro.py
+++ 3. Cronômetro/cronometro.py
@@ -4,9 +4,9 @@ import time  # Importa a biblioteca time para controlar o tempo e pausas na exec
 segundos = int(input("Defina o tempo do cronômetro (em segundos): "))
 
 # Inicia a contagem regressiva, começando pelo número de segundos informado
-for contagem in range(segundos, 0, -1):  # Loop que percorre de 'segundos' até 1, diminuindo a cada repetição
-    print(f"⏳ {contagem} segundos restantes...", end="\r")  # Exibe a contagem na mesma linha (com \r)
-    time.sleep(1)  # Pausa a execução por 1 segundo antes de continuar a contagem
+for contagem in range(segundos, 0, -1):  
+    print(f"⏳ {contagem} segundos restantes...", end="\r")  
+    time.sleep(1)  
 
 # Exibe a mensagem final quando o tempo se esgota
 print("\n⏰ Tempo esgotado!")
