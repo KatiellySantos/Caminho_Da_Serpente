c993a4d5151ae9db2d25207bd947b80eb2e91a17
Katielly Santos <katygamesof@gmail.com>
2025-06-02 12:20:26 -0300

    Update gerador_senha.py

--- 5. Gerador de senhas/gerador_senha.py
+++ 5. Gerador de senhas/gerador_senha.py
@@ -2,9 +2,9 @@ import string  # Importa o módulo 'string' para acessar caracteres alfabéticos
 import random  # Importa 'random' para gerar valores aleatórios
 
 # Define os conjuntos de caracteres que podem ser usados na senha
-letras = string.ascii_letters  # Todas as letras maiúsculas e minúsculas
-numeros = string.digits  # Todos os números de 0 a 9
-simbolos = "!@#$%^&*()"  # Símbolos válidos para a senha
+letras = string.ascii_letters  
+numeros = string.digits  
+simbolos = "!@#$%^&*()"  
 
 # Solicita o tamanho da senha ao usuário
 senha = int(input("Defina a quantidade de caracteres para a senha: "))
@@ -12,6 +12,6 @@ senha = int(input("Defina a quantidade de caracteres para a senha: "))
 # Verifica se o tamanho inserido é válido
 if senha > 0:
     senha_gerada = "".join(random.choices(letras + numeros + simbolos, k=senha))  # Gera a senha aleatória
-    print("Senha gerada:", senha_gerada)  # Exibe a senha gerada
+    print("Senha gerada:", senha_gerada)  
 else:
-    print("Erro: O tamanho da senha deve ser maior que 0.")  # Mensagem de erro para entrada inválida
+    print("Erro: O tamanho da senha deve ser maior que 0.")  
