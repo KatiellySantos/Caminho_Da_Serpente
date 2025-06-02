ba80d496a1f05234bd0f975d8bf47e549184267d
Katielly Santos <katygamesof@gmail.com>
2025-06-02 12:37:36 -0300

    Update  jogo_de_adivinhacao.py

--- 1. Jogo de Adivinhação/ jogo_de_adivinhacao.py
+++ 1. Jogo de Adivinhação/ jogo_de_adivinhacao.py
@@ -1,21 +1,21 @@
 # 1 - Configuração Inicial: Importa a biblioteca 'random' e define variáveis do jogo
 import random  # Importa a biblioteca para gerar números aleatórios
-numerosecreto = random.randint(1, 20)  # Escolhe um número aleatório entre 1 e 20
-tentativas = 0  # Inicializa o contador de tentativas
-limitetentativas = 5  # Define o número máximo de tentativas permitidas
+numerosecreto = random.randint(1, 20) 
+tentativas = 0  
+limitetentativas = 5  
 
 # 2 - Loop de tentativas: Permite ao jogador tentar adivinhar o número até o limite
-while tentativas < limitetentativas:  # O loop continua enquanto o número de tentativas for menor que o limite
-    palpite = int(input("Escolha um número de 1 a 20: "))  # Solicita um número ao usuário e converte para inteiro
-    tentativas += 1  # Incrementa o número de tentativas a cada rodada
+while tentativas < limitetentativas:  
+    palpite = int(input("Escolha um número de 1 a 20: "))  
+    tentativas += 1  
 
     if palpite < numerosecreto:  # Se o palpite for menor que o número secreto
-        print("Chute um número maior.")  # Indica que o número secreto é maior
+        print("Chute um número maior.")  
     elif palpite > numerosecreto:  # Se o palpite for maior que o número secreto
-        print("Chute um número menor.")  # Indica que o número secreto é menor
+        print("Chute um número menor.")  
     else:  # Se o palpite for igual ao número secreto
         print(f"Parabéns! Você acertou o número secreto ({numerosecreto}) em {tentativas} tentativas.")  
-        break  # Finaliza o jogo ao acertar
+        break  
 
 # Se o jogador atingir o limite de tentativas sem acertar, exibe uma mensagem final
 if tentativas == limitetentativas:  # Verifica se o jogador usou todas as tentativas sem acertar
